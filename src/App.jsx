89833075a83861cce21484c826353e4fbfa89de0
Lucien KOHLI <lucien.kohli@gmail.com>
2023-12-14 15:44:19 +0100

    secondQuest

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import PokemonCard from './components/PokemonCard'
+import { useState } from "react";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <PokemonCard />
-    </ div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
