fa4eef40a877e7b7f403638ff0cffd419c063642
Lucien KOHLI <lucien.kohli@gmail.com>
2023-12-14 16:12:26 +0100

    fourthQuest

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const PokemonCard = (props) => {
   console.log(props);
 
@@ -13,6 +15,13 @@ const PokemonCard = (props) => {
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
+};
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    imgSrc: PropTypes.string,
+    name: PropTypes.string,
+  }).isRequired,
 };
 
 export default PokemonCard;
